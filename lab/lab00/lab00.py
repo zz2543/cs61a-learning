1fd32c552882df7236c7a874d06fd1db68c40a4f
zz2543 <zzhz88247@gmail.com>
2025-08-14 18:03:15 +0800

    Add files via upload

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    return print((101 * 2 * 5 + 2 )* 2)
+
+twenty_twenty_four()
+
